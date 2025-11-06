404432b0d4160f035f20e27e76acd91ca272b311
Yan Fonseca <165041733+YanFonsecaz@users.noreply.github.com>
2025-11-05 22:09:37 -0300

    Estudos interadores

--- Atividade/iterator.py
+++ Atividade/iterator.py
@@ -15,3 +15,21 @@ class Contador:
     
 for numero in Contador(1,5):
     print(numero)
+
+class Contador2:
+    def __init__(self,num):
+        self.num = num
+        self.inicio = 0
+    
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.inicio >= len(self.num):
+            raise StopIteration
+        valor = self.num[self.inicio]
+        self.inicio += 1
+        return valor
+
+for numero in Contador2([0,20,30,40]):
+    print(numero)
